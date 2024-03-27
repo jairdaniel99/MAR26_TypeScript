e4e3db27ac517c7782940c0993a6870105369199
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-03-26 19:24:38 -0600

    explained what is SPA vs MPA

---
+++ app.ts
@@ -0,0 +1,24 @@
+// What is a Single Page Application (SPA)?
+// An application with one html page that is dynamically updated as the user interacts with the app.
+// SPA's typically use AJAX to fetch data from an API and update the page without refreshing the browser.
+
+// What is a Multi Page Application (MPA)?
+// An application with multiple html pages where the server sends a new page for each user interaction.
+// MPA's typically use server-side rendering to generate the html for each page.
+
+// What are the benefits of a SPA?
+// - Faster navigation
+// - Better user experience
+// - Less bandwidth
+// - More interactivity
+// - Better performance
+
+// What are the drawbacks of a SPA?
+// - SEO
+// - Initial load time because of all of the JavaScript
+// - Security concerns surround cross-site scripting (XSS)
+// - Memory management due to the large amount of JavaScript
+
+// What is AJAX?
+// Asynchronous JavaScript and XML
+// We can use AJAX to send and receive data(json or xml format) from a server without refreshing the page.
